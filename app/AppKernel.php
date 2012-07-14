87dc1c084ffda3560009c85b643dfc8f8f6ef90c
David Maignan <davidmaignan@gmail.com>
2012-07-14 17:20:39 -0400

    configuring sonata admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,8 @@ class AppKernel extends Kernel
             new Sonata\AdminBundle\SonataAdminBundle(),
             new Sonata\BlockBundle\SonataBlockBundle(),
             new Sonata\CacheBundle\SonataCacheBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
