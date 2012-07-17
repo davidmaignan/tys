3b90bff79666b77223221c36b9bd8e3ed4116cf9
David Maignan <davidmaignan@gmail.com>
2012-07-17 19:46:22 -0400

    admin question form on the way

--- src/Dm/AdminBundle/Controller/QuestionController.php
+++ src/Dm/AdminBundle/Controller/QuestionController.php
@@ -3,32 +3,48 @@
 namespace Dm\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
 use Dm\QuestionBundle\Entity\Question;
 use Dm\QuestionBundle\Form\QuestionType;
 
+class QuestionController extends Controller {
 
-class QuestionController extends Controller
-{
-    public function indexAction()
-    {
-        
+    public function indexAction() {
+
         $em = $this->getDoctrine()->getEntityManager();
-        
+
         $questions = $em->createQueryBuilder()
-                        ->select('b')
-                        ->from('DmQuestionBundle:Question', 'b')
-                        ->getQuery()
-                        ->getResult();
-        
-        return $this->render('DmAdminBundle:Question:index.html.twig', array('questions'=>$questions));
+                ->select('b')
+                ->from('DmQuestionBundle:Question', 'b')
+                ->getQuery()
+                ->getResult();
+
+        return $this->render('DmAdminBundle:Question:index.html.twig', array('questions' => $questions));
     }
-    
-    public function createAction(){
-        
+
+    public function createAction() {
+
+
         $question = new Question();
         $form = $this->createForm(new QuestionType(), $question);
-        
-        return $this->render('DmAdminBundle:Question:create.html.twig', array('form'=>$form->createView()));
+
+        $request = $this->getRequest();
+
+        if ($request->getMethod() == 'POST') {
+
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                
+                echo 'form valid';
+                exit;
+                
+                
+                return $this->redirect($this->generateUrl('DmAdminBundle_homepage'));
+            }
+        }
+
+        return $this->render('DmAdminBundle:Question:create.html.twig', array('form' => $form->createView()));
     }
+
 }
