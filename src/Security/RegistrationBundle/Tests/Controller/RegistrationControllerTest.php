c4cde21944bdd99be6dddf4f68d19c53897e1d23
David Maignan <davidmaignan@gmail.com>
2012-10-10 07:04:51 -0400

    Registration test completed

--- src/Security/RegistrationBundle/Tests/Controller/RegistrationControllerTest.php
+++ src/Security/RegistrationBundle/Tests/Controller/RegistrationControllerTest.php
@@ -2,10 +2,50 @@
 
 namespace Security\RegistrationBundle\Tests\Controller;
 
+//require_once dirname(__DIR__).'/../../../../app/AppKernel.php';
+
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class RegistrationControllerTest extends WebTestCase
 {
+    
+    /**
+     * @var Doctrine\ORM\EntityManager 
+     */
+    private $repo;
+    
+    /**
+     * @var Symfony\Bundle\FrameworkBundle\Routing\Router;
+     */
+    
+    private $router;
+    
+    
+    
+    
+    /**
+     * @var Symfony\Component\DependencyInjection\Container 
+     */
+    protected $container;
+    
+    
+    /**
+     * @var email 
+     */
+    private $email = 'teadsfasdfasstfgd@test.com';
+    
+    public function setUp()
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+        
+        $this->router = $kernel->getContainer()->get('router');
+        
+        //Store the container and the entity manager in test case properties
+        $this->entityManager = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+    }
+    
+    
     public function testRegistration_successful () 
     {
         $client = static::createClient();
@@ -17,13 +57,40 @@ class DefaultControllerTest extends WebTestCase
         $form = $crawler->selectButton('_register')->form();
         
         $form['fos_user_registration_form[username]'] = 'afadsfasdffdgfgd';
-        $form['fos_user_registration_form[email]'] = 'teadsfasdfasstfgd@test.com';
+        $form['fos_user_registration_form[email]'] = $this->email;
         $form['fos_user_registration_form[plainPassword][first]'] = 'camper';
         $form['fos_user_registration_form[plainPassword][second]'] = 'camper';
 
         $crawler = $client->submit($form);
         
         $this->assertTrue($crawler->filter('html:contains("Congratulations")')->count() > 0, $client->getResponse()->getContent());
+        
+        $this->repo = $this->entityManager->getRepository('MailerEmailBundle:Email');
+        
+        $this->assertEquals(1, count($this->repo->findOneBy(array('recipient'=>$this->email))));
+    }
+    
+    public function testActivation_link(){
+        
+        $email = $this->entityManager->getRepository('MailerEmailBundle:Email')->findOneBy(array('recipient'=>$this->email));
+        
+        $link = $this->router->generate('security_registration_verify', array(
+            'email'=> $this->email,
+            'activationKey' =>$email->getActivationkey()
+        ), true);
+        
+        
+        $client = static::createClient();
+        $client->followRedirects(true);
+        $crawler = $client->request('GET', $link);
+        
+        $this->assertTrue($crawler->filter('html:contains("Confirmation")')->count() > 0, $client->getResponse()->getContent());
+        
+        
+        $user = $this->entityManager->getRepository('SecurityAuthenticateBundle:User')->findOneBy(array('email'=>$this->email));
+        
+        $this->assertTrue(TRUE, $user->getConfirmed());
+        
     }
     
     public function testRegistration_confirm_password_fail () 
