145bb002d95bdea7ee1a3adf5413bab97d49a808
David Maignan <davidmaignan@gmail.com>
2013-03-30 12:51:49 -0400

    exam start page

---
+++ src/Exam/PracticeBundle/Controller/RunController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Exam\PracticeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class StartController extends Controller
+{
+    public function indexAction()
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        
+        //Retreive exam
+        $em = $this->getDoctrine()->getEntityManager();
+        $exam = $em->getRepository('ExamCoreBundle:Exam')->findByUser($user);
+        
+        return $this->render('ExamPracticeBundle:Start:index.html.twig');
+    }
+}
---
+++ src/Exam/PracticeBundle/Controller/StartController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Exam\PracticeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class StartController extends Controller
+{
+    public function indexAction()
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        
+        //Retreive exam
+        $em = $this->getDoctrine()->getEntityManager();
+        $exam = $em->getRepository('ExamCoreBundle:Exam')->findByUser($user);
+        
+        $session = $this->getRequest()->getSession();
+        
+        $session->set('exam', $exam);
+        
+        return $this->render('ExamPracticeBundle:Start:index.html.twig');
+    }
+}
